e39ff043eb4fc9a8fe89f99327a7c27bb50331e6
Thiago <thiago.dev.x@gmail.com>
2025-06-12 14:32:48 -0300

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/black-dev-x/k8s-go
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	}))
+}
