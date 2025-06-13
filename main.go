cb23c8cf63f17fc20eee176d05bd3532680650ae
Thiago <thiago.dev.x@gmail.com>
2025-06-13 15:35:16 -0300

    update

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	access_times := 0
+
+	increment := make(chan bool)
+	go func() {
+		for {
+			<-increment
+			access_times++
+		}
+	}()
+
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		increment <- true
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, Kubernetes!"))
+		w.Write([]byte(fmt.Sprintf("Access times: %d\n", access_times)))
 	}))
 }
